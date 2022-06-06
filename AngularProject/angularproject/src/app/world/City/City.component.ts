1cada288b358fa9855312905bc1d6c402526020e
ahmed3bd0008 <ahmed3bd0008@gmail.com>
2022-06-06 16:59:52 +0200

    angular paging

--- AngularProject/angularproject/src/app/world/City/City.component.ts
+++ AngularProject/angularproject/src/app/world/City/City.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import{ HttpClient }from'@angular/common/http';
+import{ HttpClient ,HttpParams}from'@angular/common/http';
 import { City } from './City';
 import { MatTableDataSource } from '@angular/material/table';
-import { MatPaginator } from '@angular/material/paginator';
+import { MatPaginator,PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-City',
@@ -15,23 +15,33 @@ export class CityComponent implements OnInit {
 private api:string=environment.apiUrl;
 public cities !: MatTableDataSource<City>;
 @ViewChild (MatPaginator)paginator !:MatPaginator
+pagEventp=new PageEvent();
 public displayedColumns: string[] = ['id', 'name', 'lat', 'lon'];
   constructor(private httpclient:HttpClient){
 
   }
 
-   ngOnInit() {
-this.getcity();
+  ngOnInit() {
+    this.pagEventp.pageIndex=1
+    this.pagEventp.pageSize=1
+    this.getcity(this.pagEventp);
    }
-   getcity(){
-    this.httpclient.get<any>(this.api+'api/City').subscribe(
+   getcity(event:PageEvent){
+     let url:string=this.api+'api/GetCityPaging';
+    let parms:HttpParams=new HttpParams();
+    parms.set("pageSize",event.pageSize);
+    parms.set("pageIndex",event.pageIndex)
+    this.httpclient.get<any>(url,{params:parms}).subscribe(
      (res:any)=>{
         console.log(res)
         console.log(res.status)
         if(!res.status){
           console.log(typeof(res.date))
-        this.cities= new MatTableDataSource<City>(res.date as City[]) ;
-        this.cities.paginator=this.paginator;
+          this.paginator.length=res.date.itemCount
+          this.paginator.pageIndex=res.date.pageIndex
+          this.paginator.pageSize=res.date.pageSize
+        this.cities= new MatTableDataSource<City>(res.date.data as City[]) ;
+       // this.cities.paginator=this.paginator;
         console.log(res.date)
         }else{
           console.log("error")
